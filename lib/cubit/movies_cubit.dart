c73fcbd373016ba33049d7bf2b601b2d1c34462e
findmebyvinay <vinaythapa762@gmail.com>
2024-05-13 15:30:30 +0545

    Comment Remove

--- lib/cubit/movies_cubit.dart
+++ lib/cubit/movies_cubit.dart
@@ -82,8 +82,7 @@ class MoviesCubit extends Cubit<MoviesState> {
     availableSeats: availableSeats,
   ));
   }
-  /*     
-  This is comment
+  /*
   if (state.selected.contains(seatIndex)) {
     // If the seat is already selected, deselect it
     selectedSeats = List.from(state.selected)..remove(seatIndex);
